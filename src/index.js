1d5e7f3d64f75168db9a7ddb81e089dce6ee8a78
aagraston <aagraston@gmail.com>
2022-02-16 13:52:25 -0800

    finished drop-down menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import "./scss/app.scss"
+import DropDown from './js/drop-down'
+import './scss/app.scss'
+
+const menus = document.querySelectorAll('.drop-down')
+const dropDown = DropDown()
+
+menus.forEach((element) => {
+  const menuToShow = element.querySelector('.drop-down-items')
+  const clickable = element.querySelector('.drop-down-button')
+
+  // here is how to bind the element
+  clickable.addEventListener('click', dropDown.toggle.bind(element, menuToShow))
+})
