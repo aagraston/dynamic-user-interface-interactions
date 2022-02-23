e0700d26f15e8574103c2e03214b0082a29a6a79
aagraston <aagraston@gmail.com>
2022-02-23 11:24:43 -0800

    added multiple js output files for multiple html files

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    dropdown: './src/dropdown.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
