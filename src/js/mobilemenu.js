fdc1082067945754b1c6cd14173cfbbbfb0b471f
aagraston <aagraston@gmail.com>
2022-02-23 16:36:21 -0800

    hamburger menu folds out from left

--- src/js/mobilemenu.js
+++ src/js/mobilemenu.js
@@ -1,1 +1,12 @@
 import '../scss/app.scss'
+import ClassToggle from './classtoggle'
+
+const hamburgerButton = document.querySelector('.hamburger-icon')
+const openableMenu = document.querySelector('.openable-menu')
+
+const revealToggle = ClassToggle('revealed')
+
+hamburgerButton.addEventListener(
+  'click',
+  revealToggle.toggle.bind(this, openableMenu)
+)
