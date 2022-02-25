6add467ad73bcd0621f35d97197e5f6c89f3aa9f
aagraston <aagraston@gmail.com>
2022-02-25 12:56:23 -0800

    Functioning, animation is off

--- src/js/mobilemenu.js
+++ src/js/mobilemenu.js
@@ -3,8 +3,22 @@ import ClassToggle from './classtoggle'
 
 const hamburgerButton = document.querySelector('.hamburger-icon')
 const openableMenu = document.querySelector('.openable-menu')
+const subMenus = document.querySelectorAll('.sub-menu')
 
 const revealToggle = ClassToggle('revealed')
+
+subMenus.forEach((element) => {
+  console.log(element)
+  const clickable = element.children[0]
+  const revealable = element.children[1]
+
+  console.log(`${clickable} ${revealable}`)
+
+  clickable.addEventListener(
+    'click',
+    revealToggle.toggle.bind(element, revealable)
+  )
+})
 
 hamburgerButton.addEventListener(
   'click',
