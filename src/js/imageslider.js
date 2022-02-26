a783e1ee1175493f33842831d337e2a2032f3d64
aagraston <aagraston@gmail.com>
2022-02-25 16:49:55 -0800

    images loading properly, slider centered on page

--- src/js/imageslider.js
+++ src/js/imageslider.js
@@ -1,3 +1,7 @@
 import '../scss/app.scss'
 
 import '../imgs/insta_01.png'
+import '../imgs/insta_02.png'
+import '../imgs/insta_03.png'
+import '../imgs/insta_04.png'
+import '../imgs/insta_05.png'
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset',
+        generator: {
+          filename: '[name][ext]',
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
